da34fdad982d403ca87af603e7e234b12c3e15e3
SHAIK MOHAMMED YASEEN <123185869+Shaik-Yaseen1@users.noreply.github.com>
2025-09-10 22:53:46 +0530

    Delete notes directory

--- notes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Note
-
-@admin.register(Note)
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ('title', 'updated_at', 'archived')
-    list_filter = ('archived',)
-    search_fields = ('title','content')
--- notes/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from .models import Note
-
-class NoteModelTest(TestCase):
-    def test_create_note(self):
-        n = Note.objects.create(title='Test', content='Hello')
-        self.assertEqual(str(n), 'Test')
-        self.assertIsNotNone(n.created_at)
